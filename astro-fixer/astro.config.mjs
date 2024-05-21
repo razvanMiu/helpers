5ea41a68dd8bc161ac862303ab74d60cbfdd6744
Miu Razvan <miu.razvan28@gmail.com>
2024-05-21 23:14:47 +0300

    Prepare fixers for other content

--- astro-fixer/astro.config.mjs
+++ astro-fixer/astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    solidJs({ devtools: true }),
+    solidJs({
+      devtools: true,
+    }),
     {
       name: "jsoneditor",
       hooks: {
@@ -15,9 +16,9 @@ export default defineConfig({
           injectScript(
             "before-hydration",
             `
-            import JSONEditor from "jsoneditor/dist/jsoneditor.min.js";
-            window.JSONEditor = JSONEditor;
-          `
+              import JSONEditor from "jsoneditor/dist/jsoneditor.min.js";
+              window.JSONEditor = JSONEditor;
+            `
           );
         },
       },
