9fdf3ab96fb576f5d7c71119a96cfed5e0df7b8e
Miu Razvan <miu.razvan28@gmail.com>
2024-05-24 18:14:16 +0300

    update

--- go-test/main.go
+++ go-test/main.go
@@ -27,19 +27,20 @@ func main() {
 		"https://admin.idralliance.global/sites/default/files/2024-04/UY.svg?changed=1715767961000",
 		"https://admin.idralliance.global/sites/default/files/2024-04/CL.svg?changed=1715767961000&w=256&q=75",
 	}
-	numGoroutines := 30 * len(urls)
+	iter := 40
+	numGoroutines := iter * len(urls)
 	ch := make(chan string, numGoroutines)
 	// List of URLs to request
 
 	// Iterate over the list of URLs and make a request to each
-	for i := 0; i < numGoroutines; i++ {
+	for i := 0; i < iter; i++ {
 		for _, url := range urls {
 			go triggerReq(url, ch)
 		}
 	}
 
 	// Receive messages from the goroutines
-	for i := 1; i <= numGoroutines; i++ {
+	for i := 0; i < numGoroutines; i++ {
 		message := <-ch
 		if strings.HasPrefix(message, "Failed") {
 			fmt.Println(message)
